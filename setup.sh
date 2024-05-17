2d56771bc392461807f0e19b070154aa3f15801d
Archisman Panigrahi <apandada1@gmail.com>
2024-05-16 23:32:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,19 @@
 
 # Created by Archisman Panigrahi (archisman-panigrahi)
 
-cp surface-rt-screen-rotator.desktop $HOME/.local/share/applications/
 mkdir $HOME/.local/share/icons
 mkdir $HOME/.local/share/icons/hicolor
 mkdir $HOME/.local/share/icons/hicolor/scalable
 mkdir $HOME/.local/share/icons/hicolor/scalable/apps
 cp screenrotator.svg $HOME/.local/share/icons/hicolor/scalable/apps/
+
+current_dir=$(pwd)
+
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Name=Screen Orientation Manager
+Exec=python3 ${current_dir}/surface-RT-screen-rotator/ScreenOrientationManager.py
+Icon=screenrotator
+Categories=Utility;
