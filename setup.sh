d7bf0c6f512351449e4ee1a57230200caa3783fc
Archisman Panigrahi <apandada1@gmail.com>
2024-05-16 23:38:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,11 @@ mkdir $HOME/.local/share/icons/hicolor/scalable/apps
 cp screenrotator.svg $HOME/.local/share/icons/hicolor/scalable/apps/
 
 current_dir=$(pwd)
+TARGET_DIR="$HOME/.local/share/applications"
+TARGET_FILE="$TARGET_DIR/surface-rt-screen-rotator.desktop"
+cd "$TARGET_DIR"
 
+cat > "$TARGET_FILE" <<EOL
 [Desktop Entry]
 Version=1.0
 Type=Application
@@ -18,3 +22,4 @@ Name=Screen Orientation Manager
 Exec=python3 ${current_dir}/surface-RT-screen-rotator/ScreenOrientationManager.py
 Icon=screenrotator
 Categories=Utility;
+EOL
